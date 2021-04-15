2589f50106a986b7676393264a5c0b1cee7f6a7a
Suyash Phatak <suyashphatakbackup@gmail.com>
2021-04-16 00:46:57 +0530

    Project Level files

---
+++ covid/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from covidapp.views import HomeView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', HomeView),
+]
